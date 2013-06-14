66eda94c051856b54e24083382b45c8a2e2fd9d4
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-06-13 22:51:44 -0300

    New bundle.

--- dist/claire.umd.js
+++ dist/claire.umd.js
@@ -32,7 +32,7 @@ return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require
   }
 }).call(this);
 
-},{"./property":2,"./generating":3,"./check":4,"./data":5}],5:[function(require,module,exports){
+},{"./generating":2,"./check":3,"./data":4,"./property":5}],4:[function(require,module,exports){
 (function(){
   var ref$, chooseInt, choose, asGenerator, choice, frequency, sequence, recursive, size, label, transform, repeat, join, char, toInteger, toUnsignedInteger, toObject, Null, Undefined, Bool, Num, Byte, Char, Str, Int, UInt, Positive, Negative, NumChar, UpperChar, LowerChar, AlphaChar, AlphaNumChar, AlphaStr, NumStr, AlphaNumStr, Id, start, chars, rest, List, Map, Nothing, Falsy, Any, slice$ = [].slice;
   ref$ = require('./random'), chooseInt = ref$.chooseInt, choose = ref$.choose;
@@ -134,7 +134,7 @@ return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require
   };
 }).call(this);
 
-},{"./random":6,"./generating":3}],6:[function(require,module,exports){
+},{"./random":6,"./generating":2}],6:[function(require,module,exports){
 (function(){
   var floor, random, choose, chooseInt, pickOne;
   floor = Math.floor, random = Math.random;
@@ -155,134 +155,6 @@ return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require
 }).call(this);
 
 },{}],2:[function(require,module,exports){
-(function(){
-  var Base, test, frozen, makeResult, invalidate, hold, reject, fail, values, validP, classify, verify, applyProperty, Property, forAll, slice$ = [].slice;
-  Base = require('boo').Base;
-  test = require('./check').test;
-  frozen = Object.freeze;
-  makeResult = curry$(function(kind, value, args, labels){
-    return {
-      kind: kind,
-      value: value,
-      labels: labels,
-      arguments: args
-    };
-  });
-  invalidate = curry$(function(args){
-    return makeResult('ignored', null, args, []);
-  });
-  hold = makeResult('held', true);
-  reject = makeResult('rejected');
-  fail = makeResult('failed');
-  values = function(it){
-    return it.map(function(it){
-      return it.value;
-    });
-  };
-  validP = curry$(function(args, prop){
-    switch (false) {
-    case prop.implications.length !== 0:
-      return true;
-    default:
-      return prop.implications.every(function(f){
-        return f.apply(null, values(args));
-      });
-    }
-  });
-  classify = curry$(function(args, prop){
-    return prop.classifiers.map(function(f){
-      return f.apply(null, values(args));
-    }).filter((function(it){
-      return it != null;
-    }));
-  });
-  verify = curry$(function(args, labels, prop){
-    var result, e;
-    try {
-      result = prop.invariant.apply(prop, values(args));
-      if (result === true) {
-        return hold(args, labels);
-      } else {
-        return reject(result, args, labels);
-      }
-    } catch (e$) {
-      e = e$;
-      return fail(e, args, labels);
-    }
-  });
-  applyProperty = curry$(function(args, prop){
-    switch (false) {
-    case !validP(args, prop):
-      return verify(args, classify(args, prop), prop);
-    default:
-      return invalidate(args);
-    }
-  });
-  Property = Base.derive({
-    init: function(args){
-      this.arguments = frozen(args) || [];
-      this.classifiers = frozen([]);
-      this.implications = frozen([]);
-      return this;
-    },
-    invariant: function(){
-      throw Error('unimplemented');
-    },
-    satisfy: function(f){
-      return this.derive({
-        invariant: f
-      });
-    },
-    classify: function(f){
-      return this.derive({
-        classifiers: this.classifiers.concat([f])
-      });
-    },
-    given: function(f){
-      return this.derive({
-        implications: this.implications.concat([f])
-      });
-    },
-    run: function(){
-      return applyProperty(this.arguments.map(function(g){
-        return g.next();
-      }), this);
-    },
-    asTest: function(config){
-      var prop;
-      prop = this;
-      return function(){
-        return test(config, prop.derive({
-          invariant: prop.invariant.bind(this)
-        }));
-      };
-    }
-  });
-  forAll = function(){
-    var as;
-    as = slice$.call(arguments);
-    return Property.make(as);
-  };
-  module.exports = {
-    Property: Property,
-    forAll: forAll
-  };
-  function curry$(f, bound){
-    var context,
-    _curry = function(args) {
-      return f.length > 1 ? function(){
-        var params = args ? args.concat() : [];
-        context = bound ? context || this : this;
-        return params.push.apply(params, arguments) <
-            f.length && arguments.length ?
-          _curry.call(context, params) : f.apply(context, params);
-      } : f;
-    };
-    return _curry();
-  }
-}).call(this);
-
-},{"./check":4,"boo":7}],3:[function(require,module,exports){
 (function(){
   var ref$, Base, derive, concatMap, replicate, pickOne, chooseInt, floor, callableP, generatorP, compute, makeValue, value, Generator, asGenerator, bind, choice, frequency, sequence, sized, recursive, label, transform, repeat, slice$ = [].slice;
   ref$ = require('boo'), Base = ref$.Base, derive = ref$.derive;
@@ -492,7 +364,7 @@ return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require
   }
 }).call(this);
 
-},{"./random":6,"boo":7,"prelude-ls":8}],4:[function(require,module,exports){
+},{"./random":6,"boo":7,"prelude-ls":8}],3:[function(require,module,exports){
 (function(){
   var makeError, Base, ref$, values, reduce, sortBy, map, frozen, keys, round, defaultConfig, EFailure, EAbandoned, percentage, withDefaults, status, failedP, describeVerdict, describeIgnored, labelHistogram, describeFailures, describeReport, Report, check, test;
   makeError = require('flaw');
@@ -730,7 +602,135 @@ return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require
   }
 }).call(this);
 
-},{"flaw":9,"boo":7,"prelude-ls":8}],7:[function(require,module,exports){
+},{"flaw":9,"boo":7,"prelude-ls":8}],5:[function(require,module,exports){
+(function(){
+  var Base, test, frozen, makeResult, invalidate, hold, reject, fail, values, validP, classify, verify, applyProperty, Property, forAll, slice$ = [].slice;
+  Base = require('boo').Base;
+  test = require('./check').test;
+  frozen = Object.freeze;
+  makeResult = curry$(function(kind, value, args, labels){
+    return {
+      kind: kind,
+      value: value,
+      labels: labels,
+      arguments: args
+    };
+  });
+  invalidate = curry$(function(args){
+    return makeResult('ignored', null, args, []);
+  });
+  hold = makeResult('held', true);
+  reject = makeResult('rejected');
+  fail = makeResult('failed');
+  values = function(it){
+    return it.map(function(it){
+      return it.value;
+    });
+  };
+  validP = curry$(function(args, prop){
+    switch (false) {
+    case prop.implications.length !== 0:
+      return true;
+    default:
+      return prop.implications.every(function(f){
+        return f.apply(null, values(args));
+      });
+    }
+  });
+  classify = curry$(function(args, prop){
+    return prop.classifiers.map(function(f){
+      return f.apply(null, values(args));
+    }).filter((function(it){
+      return it != null;
+    }));
+  });
+  verify = curry$(function(args, labels, prop){
+    var result, e;
+    try {
+      result = prop.invariant.apply(prop, values(args));
+      if (result === true) {
+        return hold(args, labels);
+      } else {
+        return reject(result, args, labels);
+      }
+    } catch (e$) {
+      e = e$;
+      return fail(e, args, labels);
+    }
+  });
+  applyProperty = curry$(function(args, prop){
+    switch (false) {
+    case !validP(args, prop):
+      return verify(args, classify(args, prop), prop);
+    default:
+      return invalidate(args);
+    }
+  });
+  Property = Base.derive({
+    init: function(args){
+      this.arguments = frozen(args) || [];
+      this.classifiers = frozen([]);
+      this.implications = frozen([]);
+      return this;
+    },
+    invariant: function(){
+      throw Error('unimplemented');
+    },
+    satisfy: function(f){
+      return this.derive({
+        invariant: f
+      });
+    },
+    classify: function(f){
+      return this.derive({
+        classifiers: this.classifiers.concat([f])
+      });
+    },
+    given: function(f){
+      return this.derive({
+        implications: this.implications.concat([f])
+      });
+    },
+    run: function(){
+      return applyProperty(this.arguments.map(function(g){
+        return g.next();
+      }), this);
+    },
+    asTest: function(config){
+      var prop;
+      prop = this;
+      return function(){
+        return test(config, prop.derive({
+          invariant: prop.invariant.bind(this)
+        }));
+      };
+    }
+  });
+  forAll = function(){
+    var as;
+    as = slice$.call(arguments);
+    return Property.make(as);
+  };
+  module.exports = {
+    Property: Property,
+    forAll: forAll
+  };
+  function curry$(f, bound){
+    var context,
+    _curry = function(args) {
+      return f.length > 1 ? function(){
+        var params = args ? args.concat() : [];
+        context = bound ? context || this : this;
+        return params.push.apply(params, arguments) <
+            f.length && arguments.length ?
+          _curry.call(context, params) : f.apply(context, params);
+      } : f;
+    };
+    return _curry();
+  }
+}).call(this);
+
+},{"./check":3,"boo":7}],7:[function(require,module,exports){
 // # Module boo
 //
 // Base primitives for prototypical OO.
